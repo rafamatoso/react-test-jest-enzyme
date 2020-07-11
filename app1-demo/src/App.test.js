95655f021bf1e89ab5ee502a214203189e87fdac
Rafael Matoso <rafaelmatosog@gmail.com>
2020-07-11 20:37:23 -0300

    L10: Using Enzyme in a test

--- app1-demo/src/App.test.js
+++ app1-demo/src/App.test.js
@@ -1,9 +1,15 @@
 import React from "react";
-import Enzyme from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 // First argument: name os test suite; Second argument: anonymous function that executes the tests
-test("renders learn react link", () => {});
+test("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
+
+  // debug returns a HTML-like of component DOM
+  // console.log(wrapper.debug());
+});
