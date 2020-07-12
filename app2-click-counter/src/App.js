76819c3c2db553562c01b4b3e4778ca2f38f6942
Rafael Matoso <rafaelmatosog@gmail.com>
2020-07-11 22:10:24 -0300

    L18, L19: More data-test attributes tests

--- app2-click-counter/src/App.js
+++ app2-click-counter/src/App.js
@@ -5,8 +5,10 @@ export default class App extends Component {
   render() {
     return (
       <>
-        <div data-test="component-app"></div>
-        <div data-test="component-app"></div>
+        <div data-test="component-app">
+          <h1 data-test="counter-display">The counter is currently</h1>
+          <button data-test="increment-button">Increment Counter</button>
+        </div>
       </>
     );
   }
--- app2-click-counter/src/App.test.js
+++ app2-click-counter/src/App.test.js
@@ -8,12 +8,20 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
 test("renders without error", () => {
   const wrapper = shallow(<App></App>);
   const appComponent = wrapper.find("[data-test='component-app']");
-  expect(appComponent.length).toBe(2);
+  expect(appComponent.length).toBe(1);
 });
 
-test("renders increment button", () => {});
+test("renders increment button", () => {
+  const wrapper = shallow(<App></App>);
+  const button = wrapper.find("[data-test='increment-button']");
+  expect(button.length).toBe(1);
+});
 
-test("renders counter display", () => {});
+test("renders counter display", () => {
+  const wrapper = shallow(<App></App>);
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  expect(counterDisplay.length).toBe(1);
+});
 
 test("should counter starts at 0", () => {});
 
