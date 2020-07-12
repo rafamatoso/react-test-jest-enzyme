15d3306f78828f169a31828af3977e19ebfd6276
Rafael Matoso <rafaelmatosog@gmail.com>
2020-07-11 21:15:31 -0300

    L11 to L13

--- app1-demo/src/App.test.js
+++ app1-demo/src/App.test.js
@@ -5,11 +5,7 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-// First argument: name os test suite; Second argument: anonymous function that executes the tests
 test("renders without crashing", () => {
   const wrapper = shallow(<App />);
   expect(wrapper).toBeTruthy();
-
-  // debug returns a HTML-like of component DOM
-  // console.log(wrapper.debug());
 });
