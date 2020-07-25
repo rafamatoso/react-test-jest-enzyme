a7fe241358a253a862ffe23b1de0719e490a3c81
Rafael Matoso <rafaelmatosog@gmail.com>
2020-07-25 18:57:34 -0300

    L66 to L70: Install Redux Thunk middleware

--- app3-jotto/app-jotto/test/testUtils.js
+++ app3-jotto/app-jotto/test/testUtils.js
@@ -1,7 +1,8 @@
 import checkPropTypes from "check-prop-types";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import rootReducer from "../src/reducers";
+import { middlewares } from "../src/store/configureStore";
 
 /**
  * Create a testing store with imported reducers, middleware, and initial state.
@@ -11,13 +12,16 @@ import rootReducer from "../src/reducers";
  * @returns {Store} - Redux store.
  */
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+    createStore
+  );
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 /**
  * Return ShallowWrapper containing node(s) with the given data-test value.
  * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within.
- * @param {string} val - Value of data-test attribute for search
+ * @param {string} val - Value of data-test attribute for search.
  * @returns {ShallowWrapper}
  */
 export const findByTestAttr = (wrapper, val) => {
