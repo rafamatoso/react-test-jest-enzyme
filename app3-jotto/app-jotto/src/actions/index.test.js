db63c6edbd861955f4ca0dca7bd321e84b02fad5
Rafael Matoso <rafaelmatosog@gmail.com>
2020-07-25 20:02:50 -0300

    L71, L72: Set up guessWords tests

--- app3-jotto/app-jotto/src/actions/index.test.js
+++
@@ -1,8 +0,0 @@
-import { correctGuess, actionsTypes } from "./";
-
-describe("correctGuess", () => {
-  test("returns an action with type `CORRECT_GUESS`", () => {
-    const action = correctGuess();
-    expect(action).toEqual({ type: actionsTypes.CORRECT_GUESS });
-  });
-});
---
+++ app3-jotto/app-jotto/src/integration.test.js
@@ -0,0 +1,16 @@
+import { storeFactory } from "../test/testUtils";
+import { guessWord } from "./actions";
+
+describe("correctGuess action dispatcher", () => {
+  describe("no guessed words", () => {
+    test("updates state correctly for unsuccesful guess", () => {});
+
+    test("updates state correctly for succesful guess", () => {});
+  });
+
+  describe("some guessed words", () => {
+    test("updates state correctly for unsuccesful guess", () => {});
+
+    test("updates state correctly for succesful guess", () => {});
+  });
+});
