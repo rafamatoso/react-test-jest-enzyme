81b33670fa88f262e05e07c6f86ab15989c4d6c8
Rafael Matoso <rafaelmatosog@gmail.com>
2020-07-25 21:17:01 -0300

    L73 to L76: Add Thunk for 'guessedWorks' piece of state

--- app3-jotto/app-jotto/src/actions/index.js
+++ app3-jotto/app-jotto/src/actions/index.js
@@ -1,3 +1,5 @@
+import { getLetterMatchCount } from "../helpers";
+
 export const actionsTypes = {
   CORRECT_GUESS: "CORRECT_GUESS",
   GUESS_WORD: "GUESS_WORD",
@@ -11,5 +13,17 @@ export const actionsTypes = {
  * @returns {function} - Redux Thunk function.
  */
 export const guessWord = (guessedWord) => {
-  return function (dispatch, getState) {};
+  return function (dispatch, getState) {
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+
+    dispatch({
+      type: actionsTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount },
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({ type: actionsTypes.CORRECT_GUESS });
+    }
+  };
 };
--- app3-jotto/app-jotto/src/integration.test.js
+++ app3-jotto/app-jotto/src/integration.test.js
@@ -2,15 +2,95 @@ import { storeFactory } from "../test/testUtils";
 import { guessWord } from "./actions";
 
 describe("correctGuess action dispatcher", () => {
-  describe("no guessed words", () => {
-    test("updates state correctly for unsuccesful guess", () => {});
+  const secretWord = "party";
+  const unsuccessfulGuess = "train";
 
-    test("updates state correctly for succesful guess", () => {});
+  describe("no guessed words", () => {
+    let store;
+
+    const initialState = { secretWord };
+
+    beforeEach(() => {
+      store = storeFactory(initialState);
+    });
+
+    test("updates state correctly for unsuccesful guess", () => {
+      store.dispatch(guessWord(unsuccessfulGuess));
+      const newState = store.getState();
+
+      const expectedState = {
+        ...initialState,
+        success: false,
+        guessedWords: [
+          {
+            guessedWord: unsuccessfulGuess,
+            letterMatchCount: 3,
+          },
+        ],
+      };
+
+      expect(newState).toEqual(expectedState);
+    });
+
+    test("updates state correctly for successful guess", () => {
+      store.dispatch(guessWord(secretWord));
+      const newState = store.getState();
+
+      const expectedState = {
+        secretWord,
+        success: true,
+        guessedWords: [
+          {
+            guessedWord: secretWord,
+            letterMatchCount: 5,
+          },
+        ],
+      };
+
+      expect(newState).toEqual(expectedState);
+    });
   });
 
   describe("some guessed words", () => {
-    test("updates state correctly for unsuccesful guess", () => {});
+    const guessedWords = [{ guessedWord: "agile", letterMatchCount: 1 }];
+    const initialState = { guessedWords, secretWord };
 
-    test("updates state correctly for succesful guess", () => {});
+    let store;
+
+    beforeEach(() => {
+      store = storeFactory(initialState);
+    });
+
+    test("updates state correctly for unsuccesful guess", () => {
+      store.dispatch(guessWord(unsuccessfulGuess));
+      const newState = store.getState();
+
+      const expectedState = {
+        secretWord,
+        success: false,
+        guessedWords: [
+          ...guessedWords,
+          { guessedWord: unsuccessfulGuess, letterMatchCount: 3 },
+        ],
+      };
+
+      expect(newState).toEqual(expectedState);
+    });
+
+    test("updates state correctly for succesful guess", () => {
+      store.dispatch(guessWord(secretWord));
+      const newState = store.getState();
+
+      const expectedState = {
+        secretWord,
+        success: true,
+        guessedWords: [
+          ...guessedWords,
+          { guessedWord: secretWord, letterMatchCount: 5 },
+        ],
+      };
+
+      expect(newState).toEqual(expectedState);
+    });
   });
 });
