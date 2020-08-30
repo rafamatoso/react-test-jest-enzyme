513417884402dc4ff8277c70ce22bfd44fed7cbf
Rafael Matoso <rafaelmatosog@gmail.com>
2020-08-29 22:44:03 -0300

    L97: Updates in App.js

--- app3-jotto/app-jotto/src/App.js
+++ app3-jotto/app-jotto/src/App.js
@@ -3,11 +3,22 @@ import { connect } from "react-redux";
 
 import "./App.css";
 
+import TotalGuesses from "./components/TotalGuesses";
+import NewWordButton from "./components/NewWordButton";
+import SecretWordReveal from "./components/SecretWordReveal";
+import EnterWordButton from "./components/EnterWordButton";
+import EnterWordForm from "./components/EnterWordForm";
+import ServerError from "./components/ServerError";
 import GuessedWords from "./components/GuessedWords";
 import Congrats from "./components/Congrats";
 import Input from "./components/Input";
-import TotalGuesses from "./components/TotalGuesses";
-import { getSecretWord } from "./actions";
+
+import {
+  getSecretWord,
+  resetGame,
+  setUserSecretWord,
+  setUserEntering,
+} from "./actions";
 
 export class UnconnectedApp extends Component {
   /**
@@ -15,30 +26,69 @@ export class UnconnectedApp extends Component {
    * @returns {undefined}
    */
   componentDidMount() {
-    // Get the secret word
     this.props.getSecretWord();
   }
 
   render() {
+    let contents;
+
+    if (this.props.serverError) {
+      contents = <ServerError />;
+    } else if (this.props.userEnter === "inProgress") {
+      contents = <EnterWordForm formAction={this.props.setUserSecretWord} />;
+    } else {
+      contents = (
+        <div>
+          <Congrats success={this.props.success} />
+
+          <SecretWordReveal
+            display={this.props.gaveUp}
+            secretWord={this.props.secretWord}
+          />
+
+          <NewWordButton
+            display={this.props.success || this.props.gaveUp}
+            resetAction={this.props.resetGame}
+          />
+
+          <Input />
+
+          <GuessedWords guessedWords={this.props.guessedWords} />
+
+          <TotalGuesses guessCount={this.props.guessedWords.length} />
+
+          <EnterWordButton
+            display={this.props.guessedWords.length === 0}
+            buttonAction={this.props.setUserEntering}
+          />
+        </div>
+      );
+    }
     return (
       <div className="container">
-        <h1>
-          {"<"} Jotto {">"}
-        </h1>
-        <Congrats success={this.props.success} />
-        <Input />
-        <GuessedWords guessedWords={this.props.guessedWords} />
-        {this.props.guessedWords.length > 0 && (
-          <TotalGuesses guessesCount={this.props.guessedWords.length} />
-        )}
+        <h1>Jotto</h1>
+        {contents}
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  const { success, guessedWords, secretWord } = state;
-  return { success, guessedWords, secretWord };
+const mapStateToProps = ({
+  success,
+  guessedWords,
+  secretWord,
+  gaveUp,
+  userEnter,
+  serverError,
+}) => {
+  return { success, guessedWords, secretWord, gaveUp, userEnter, serverError };
 };
 
-export default connect(mapStateToProps, { getSecretWord })(UnconnectedApp);
+const actions = {
+  getSecretWord,
+  resetGame,
+  setUserSecretWord,
+  setUserEntering,
+};
+
+export default connect(mapStateToProps, actions)(UnconnectedApp);
