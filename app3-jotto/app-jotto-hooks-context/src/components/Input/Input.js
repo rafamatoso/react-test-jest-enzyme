b0e642f9a7d4ed4ae9dabb1fb37f6b2a4c60ea3c
Rafael Matoso <rafaelmatosog@gmail.com>
2020-09-13 20:41:07 -0300

    L105: Input props with checkProps test

--- app3-jotto/app-jotto-hooks-context/src/components/Input/Input.js
+++ app3-jotto/app-jotto-hooks-context/src/components/Input/Input.js
@@ -1,5 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export const Input = (props) => {
+export const Input = ({ secretWord }) => {
   return <div data-test="component-input" />;
+};
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
 };
--- app3-jotto/app-jotto-hooks-context/src/components/Input/Input.test.js
+++ app3-jotto/app-jotto-hooks-context/src/components/Input/Input.test.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { findByTestAttr } from "../../../test/testUtils";
+
+import { findByTestAttr, checkProps } from "../../../test/testUtils";
 import { Input } from "./Input";
 
 /**
  * Setup function for input component.
  * @returns {ShallowWrapper}
  */
-const setup = () => {
-  return shallow(<Input />);
+const setup = (secretWord = "party") => {
+  return shallow(<Input secretWord={secretWord} />);
 };
 
 test("Input renders without error", () => {
   const wrapper = setup();
-  const component = findByTestAttr(wrapper, "component-input");
+  const inputComponent = findByTestAttr(wrapper, "component-input");
 
-  expect(component.length).toBe(1);
+  expect(inputComponent.length).toBe(1);
+});
+
+test("does not throw warning with expected props", () => {
+  checkProps(Input, { secretWord: "party" });
 });
