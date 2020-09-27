54a580426fd397856c35e0b758a50867f8b0590c
Rafael Matoso <rafaelmatosog@gmail.com>
2020-09-27 16:31:00 -0300

    L106 to L110: State-controlled field with test

--- app3-jotto/app-jotto-hooks-context/src/components/Input/Input.js
+++ app3-jotto/app-jotto-hooks-context/src/components/Input/Input.js
@@ -2,7 +2,31 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export const Input = ({ secretWord }) => {
-  return <div data-test="component-input" />;
+  const [currentGuess, setCurrentGuess] = React.useState("");
+
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          onClick={(e) => {
+            e.preventDefault();
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 Input.propTypes = {
