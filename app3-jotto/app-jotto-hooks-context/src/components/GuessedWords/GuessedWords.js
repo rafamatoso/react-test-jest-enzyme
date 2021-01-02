656ddad85959d351ea0d80dacf8a90e5f2e34f2d
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-02 20:39:05 -0300

    L142 to L144: GuessedWords Language with tests

--- app3-jotto/app-jotto-hooks-context/src/components/GuessedWords/GuessedWords.js
+++ app3-jotto/app-jotto-hooks-context/src/components/GuessedWords/GuessedWords.js
@@ -1,11 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import stringsModule from "../../helpers/language/language";
+import languageContext from "../../context/languageContext";
+
 const GuessedWords = (props) => {
+  const language = React.useContext(languageContext);
   let contents;
+
   if (props.guessedWords.length === 0) {
     contents = (
-      <span data-test="guess-instructions">Try to guess the secret word!</span>
+      <span data-test="guess-instructions">
+        {stringsModule.getStringByLanguage(language, "guessPrompt")}
+      </span>
     );
   } else {
     const guessedWordsRows = props.guessedWords.map((word, index) => (
@@ -14,14 +21,25 @@ const GuessedWords = (props) => {
         <td>{word.letterMatchCount}</td>
       </tr>
     ));
+
     contents = (
       <div data-test="guessed-words">
-        <h3>Guessed Words</h3>
+        <h3>{stringsModule.getStringByLanguage(language, "guessedWords")}</h3>
         <table className="table table-sm">
           <thead className="thead-light">
             <tr>
-              <th>Guess</th>
-              <th>Matching Letters</th>
+              <th>
+                {stringsModule.getStringByLanguage(
+                  language,
+                  "guessColumnHeader"
+                )}
+              </th>
+              <th>
+                {stringsModule.getStringByLanguage(
+                  language,
+                  "matchingLettersColumnHeader"
+                )}
+              </th>
             </tr>
           </thead>
           <tbody>{guessedWordsRows}</tbody>
@@ -29,6 +47,7 @@ const GuessedWords = (props) => {
       </div>
     );
   }
+
   return <div data-test="component-guessed-words">{contents}</div>;
 };
 
