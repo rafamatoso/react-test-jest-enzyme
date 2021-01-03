a6c833a0ae81667a8c9420b1a3496b38319c337c
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-03 19:21:31 -0300

    feat: implemented using a Wordnik key or requesting a local server

--- app3-jotto/app-jotto-hooks-context/src/actions/hookActions.js
+++ app3-jotto/app-jotto-hooks-context/src/actions/hookActions.js
@@ -2,12 +2,19 @@ import axios from "axios";
 
 import { wordnikKey } from "../config/wordnikKey";
 
-export const WORDNIK_URL = `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=1000&minDictionaryCount=100&maxDictionaryCount=-1&minLength=5&maxLength=5&api_key=${wordnikKey}`;
+let URL;
+const LOCALHOST = "http://localhost:3030";
+
+if (wordnikKey) {
+  URL = `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=1000&minDictionaryCount=100&maxDictionaryCount=-1&minLength=5&maxLength=5&api_key=${wordnikKey}`;
+} else {
+  URL = LOCALHOST;
+}
 
 export const getSecretWord = async (setSecretWord) => {
-  const response = await axios.get(WORDNIK_URL);
+  const response = await axios.get(URL);
 
-  setSecretWord(response.data.word);
+  wordnikKey ? setSecretWord(response.data.word) : setSecretWord(response.data);
 };
 
 // default export for mocking convenience
